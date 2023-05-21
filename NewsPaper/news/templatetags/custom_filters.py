e7d21d33159b89e1fc5e19a7e136ec8d8cdc3e78
eartzhi <zhigunovam@gmail.com>
2023-05-21 23:53:10 +0300

    Смотреть ссылки posts и posts/1...5

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -4,8 +4,10 @@ register = template.Library()
 
 
 @register.filter()
-def censored(text: str, vocabulary = ['долг', 'Долг', 'дефиц',
-                                 'Дефиц', 'беда',  'Беда']) -> str:
+def censored(text: str, vocabulary=None) -> str:
+    if vocabulary is None:
+        vocabulary = ['долг', 'Долг', 'дефиц',
+                      'Дефиц', 'беда', 'Беда']
     for word in vocabulary:
         text = text.replace(word, '*' * len(word))
     return text
