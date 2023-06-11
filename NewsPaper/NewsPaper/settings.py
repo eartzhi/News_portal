90cb50478be4f6f1eb9d17e7f036fe3b75f64c5c
eartzhi <zhigunovam@gmail.com>
2023-06-12 00:18:05 +0300

    module D9 final

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from .constants import EMAIL_PASSWORD
+
 
 load_dotenv()
 env_path = Path('.')/'.env'
--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -63,7 +63,7 @@ class Command(BaseCommand):
         # добавляем работу нашему задачнику
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(day_of_week='mon', hour='00', minute='13'),
+            trigger=CronTrigger(day_of_week='mon', hour='00', minute='17'),
             # То же, что и интервал, но задача тригера таким образом более понятна django
             id="my_job",  # уникальный айди
             max_instances=1,
