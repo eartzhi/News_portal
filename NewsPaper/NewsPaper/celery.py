cf690099a817e3ed755e71e19471a4a1498d85db
eartzhi <zhigunovam@gmail.com>
2023-06-19 11:07:58 +0300

    module D10 final commit Docker without

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -6,10 +6,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
 app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.timezone = 'Europe/Moscow'
 app.conf.beat_schedule = {
     'every_week_notification':{
         'task': 'news.tasks.weekly_notificator',
-        'schedule': crontab(minute=0, hour=8, day_of_week='monday'),
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
         'args': (),
     }
 }
