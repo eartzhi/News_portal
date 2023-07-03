f2c8d18f85bcb7847af5855d5d551ae373a83c1b
eartzhi <zhigunovam@gmail.com>
2023-07-03 15:51:56 +0300

    tests CraeteView commit

--- NewsPaper/news/tests.py
+++ NewsPaper/news/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase, RequestFactory
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.test import Client
 from django.contrib.auth.models import User, AnonymousUser, Group, Permission
 from .views import Account, NewsCreate
 from .models import Author, Post, Category
+from .forms import PostsForm
 from django.utils import timezone
 
 
@@ -24,59 +25,54 @@ class SimpleTest(TestCase):
         premium_group.permissions.add(can_add_post)
         premium_group.permissions.add(can_change_post)
         self.user1.groups.add(premium_group)
-        Category.objects.create(category='Политика')
-        # cat = Category.objects.get(category='Политика')
-        # my_post = Post.objects.create(author=self.auth,
-        #                              header="gbdgbdfbhsrth",
-        #                              text= 'wrgehggstndrtnryndryn4eh' )
-        # # my_post.category.set([cat])
+        self.cat = Category.objects.create(category='Политика')
+        self.my_post = Post.objects.create(author=self.auth,
+                                     header="gbdgbdfhglkgbhsrth",
+                                     text= 'wrgehggsfukfyuktndrtnryndryn4eh' )
+        self.my_post.category.set([self.cat])
 
-    # def test_user_check(self):
-    #     request1 = self.factory.get(reverse('account',
-    #                                          kwargs={'pk': self.user1.id}))
-    #     request1.user = self.user1
-    #     response1 = Account.as_view()(request1, pk=self.user1.id)
-    #     print(response1)
-    #     self.assertEqual(response1.status_code, 200)
-    #     request2 = self.factory.post(
-    #         reverse('account', kwargs={'pk': self.user1.id}),
-    #         {'username': 'John1',
-    #          'first_name': 'John1',
-    #          'last_name': 'John_last_name'})
-    #     request2.user = self.user1
-    #     response2 = Account.as_view(success_url='search')(request2, pk=self.user1.id)
-    #     print(response2)
-    #     self.assertEqual(response2.status_code, 302)
-    #     self.user1.refresh_from_db()
-    #     self.assertEqual(self.user1.username, 'John1')
-    #     self.assertEqual(self.user1.first_name, 'John1')
-    #     self.assertEqual(self.user1.last_name, 'John_last_name')
+    def test_user_check(self):
+        request1 = self.factory.get(reverse('account',
+                                             kwargs={'pk': self.user1.id}))
+        request1.user = self.user1
+        response1 = Account.as_view()(request1, pk=self.user1.id)
+        self.assertEqual(response1.status_code, 200)
+        request2 = self.factory.post(
+            reverse('account', kwargs={'pk': self.user1.id}),
+            {'username': 'John1',
+             'first_name': 'John1',
+             'last_name': 'John_last_name'})
+        request2.user = self.user1
+        response2 = Account.as_view(success_url='search')(request2, pk=self.user1.id)
+        self.assertEqual(response2.status_code, 302)
+        self.user1.refresh_from_db()
+        self.assertEqual(self.user1.username, 'John1')
+        self.assertEqual(self.user1.first_name, 'John1')
+        self.assertEqual(self.user1.last_name, 'John_last_name')
+
+    def test_form(self):
+        self.form_data = {
+                    'text': 'wrgehggstndrtnsdthdrthdrtftyjfyjftyftyjryndryn4eh',
+                    "header": "gbdgbdftyjftyjtfyjytjjftyjftyfyfyfyfyftyjbhsrth",
+                    'author': self.auth,
+                    'category': self.my_post.category.all(),
+                    'post_type': "N",
+                     }
+        form = PostsForm(data=self.form_data)
+        self.assertTrue(form.is_valid())
 
     def test_post_create(self):
-        # auth = Author.objects.get(nick_name=self.user1.username)
-        cat = Category.objects.get(category='Политика')
-        request = self.factory.post('posts/news/create',
-                                    {
-                                        'text': 'wrgehggstndrtnsdthdrthdrthdrthdtrdhdrthrthdrthdrthdrthrthrthdrthdtrhdrthdrthftyjftyjftftyjfyjftyftyjryndryn4eh',
-                                        "header": "gbdgbdftyjftyjtfyjytjtyjftyjftyfyfyfyfyftyjbhsrth",
-                                        'category': 'Политика',
-                                        'author': self.auth,
-                                        'post_type': "N",
-                                     # "header": "gbdgbdftyjftyjtfyjytjtyjftyjftyfyfyfyfyftyjbhsrth",
-                                     # 'text': 'wrgehggstndrtnsdthdrthdrthdrthdtrdhdrthrthdrthdrthdrthrthrthdrthdtrhdrthdrthftyjftyjftftyjfyjftyftyjryndryn4eh',
-                                     # # 'creation_time': timezone.now(),
+        request = self.factory.post(reverse('create_news'),
+                                    data={
+                                    'author': 1,
+                                    'category': 1,
+                                    "header": "gbdgbdftyjftyjtfyjytjjftyjfty",
+                                    'text': 'wrgehggstndrtnsdthdrthdrtftyjfy',
                                      })
-        # self.Post.refresh_from_db()
         request.user = self.user1
-        print(request.headers)
         response = NewsCreate.as_view()(request)
-        # response.render()
-        print(response)
-        self.assertEqual(response.status_code, 200)
-        new_news = Post.objects.all()
-        print(new_news)
-        # self.assertEquals(new_news.count(), 1)
-        # self.assertEqual(new_news.values('category__category'), cat)
-        # self.assertEqual(new_news.header, 'gbdgbdfbhsrth')
-        # self.assertEqual(new_news.text, 'wrgehggstndrtnryndryn4eh')
-
+        self.assertEqual(response.status_code, 302)
+        new_news = Post.objects.last()
+        self.assertEquals(Post.objects.count(), 2)
+        self.assertEqual(new_news.header, 'gbdgbdftyjftyjtfyjytjjftyjfty')
+        self.assertEqual(new_news.text, 'wrgehggstndrtnsdthdrthdrtftyjfy')
